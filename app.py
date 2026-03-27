cdd965b904f609520264e9846049358fd0cab993
martin-arcpi <104268393+martin-arcpi@users.noreply.github.com>
2026-03-27 15:21:02 +0000

    Created user and password generation capability.

---
+++ app.py
@@ -0,0 +1,27 @@
+import sqlite3
+    
+users = {}
+
+user_input = ""
+password = ""
+
+while user_input != "done":
+    print("Welcome to the registration system!\n")
+    user_input = input("Please enter the username you wish to use to register:   ")
+    password = input("Please enter the password that you would like to use:     ")
+
+    users[user_input] = password
+
+    user_input = input("Would you like to add another user?      ")
+    if user_input == "no":
+        break
+    elif user_input == "yes":
+        continue
+for user, password in users.items():
+    print(f"The user: {user}    password:   {password}")
+
+
+with sqlite3.connect("db.sqlite3") as conn:
+    pass
+
+
